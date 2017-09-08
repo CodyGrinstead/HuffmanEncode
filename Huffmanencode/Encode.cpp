38eec2aeddf3b1367bb78097221add1f08f55ab0
CodyGrinstead <yodacodyone@gmail.com>
2017-09-08 12:20:21 -0600

    Init Commit
    Initial commit
    creation of cpp files
    creation of repo

---
+++ Huffmanencode/Encode.cpp
@@ -0,0 +1,17 @@
+//Cody Grinstead
+// CSCI 480
+// Huffman Encode
+#include <fstream>
+#include <iomanip>
+#include<iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+	system("pause");
+	return 0;
+
+}
